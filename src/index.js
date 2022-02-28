ca101fad62e724d8c46b8f03f1af0a7ae15ccd49
Daniel Ufeli <danielufeli@yahoo.com>
2022-02-28 17:21:21 +0100

    Update folder structure

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import configureStore from './redux/configureStore';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './components/App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <Provider store={configureStore}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
---
+++ src/redux/books/Books.js
@@ -0,0 +1,35 @@
+import { v4 as uuidv4 } from 'uuid';
+const ADD_BOOK = 'bookStore/books/ADD_BOOK';
+
+const initialState = [
+  {
+    id: uuidv4(),
+    name: 'The Hunger Games',
+    category: 'Action',
+    author: 'Suzanne Collins',
+  },
+  {
+    id: uuidv4(),
+    name: 'Dune',
+    category: 'Science Fiction',
+    author: 'Suzanne Collins',
+  },
+  {
+    id: uuidv4(),
+    name: 'Capital in the Twenty-First Century',
+    category: 'economy',
+    author: 'Frank Herbert',
+  },
+];
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+      case ADD_BOOK:
+        return [...state, action.payload];
+  
+      default:
+        return state;
+    }
+  };
+  
+  export default reducer;
